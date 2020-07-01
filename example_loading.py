deb924848c206b2cc7424ca221067934429c4bf8
Shrutiben NAIK <sn254804@is153937.intra.cea.fr>
2020-07-01 19:37:02 +0200

    adding imports

--- example_loading.py
+++ example_loading.py
@@ -1,4 +1,4 @@
-
+from variability import flyby_dist 
 templatefile = '~/Desktop/Backup_05_11/Milad_data/templates/templates_combined.mat'
 datapath = '/home/sn254804/Desktop/Backup_05_11/Milad_data/datafiles/'
 resultpath = '/home/sn254804/Desktop/Backup_05_11/Milad_data/flybys/'
